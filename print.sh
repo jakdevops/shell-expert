6b3c3ee42ee3da8667b3a717771640cfcfb44b99
Welcome <jakdevops@gmail.com>
2023-07-24 00:51:35 -0700

    Roboshop scripts catalogue headings

--- print.sh
+++ print.sh
@@ -1,1 +1,21 @@
 echo helloworld
+
+#red    31
+#green  32
+#yellow 33
+#blue   34
+#magenta 35
+#cyan   36
+
+#syntax
+#echo -e "\e[CODEmMESSAGE\e[0m"
+
+echo -e "\e[31mHELLOW WORLD\e[0m"
+echo -e "\e"[32mhelow world\e[0m"
+echo -e "\e[33mhelooworld\e[0m"
+echo -e "\e[34mheloo world\e[0m"
+echo -e "\e[35mheloworld\e[0m"
+echo -e "\e[36mhelloworld\e[0m
+
+
+
