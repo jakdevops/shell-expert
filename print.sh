f342a151b6bf61f7b6171f518aec624567e17db8
Welcome <jakdevops@gmail.com>
2023-07-24 06:02:26 -0700

    Roboshop scripts printing colors

--- print.sh
+++ print.sh
@@ -1,4 +1,4 @@
-echo helloworld
+echo -e "\e[32m -e "\e[32m helloworld
 
 #red    31
 #green  32
@@ -8,14 +8,14 @@ echo helloworld
 #cyan   36
 
 #syntax
-#echo -e "\e[CODEmMESSAGE\e[0m"
+#echo -e "\e[32m -e "\e[32m -e "\e[CODEmMESSAGE\e[0m"
 
-echo -e "\e[31mHELLOW WORLD\e[0m"
-echo -e "\e[32mhelow world\e[0m"
-echo -e "\e[33mhelooworld\e[0m"
-echo -e "\e[34mheloo world\e[0m"
-echo -e "\e[35mheloworld\e[0m"
-echo -e "\e[36mhelloworld\e[0m"
+echo -e "\e[32m -e "\e[31mHELLOW WORLD\e[0m"
+echo -e "\e[32m -e "\e[32mhelow world\e[0m"
+echo -e "\e[32m -e "\e[33mhelooworld\e[0m"
+echo -e "\e[32m -e "\e[34mheloo world\e[0m"
+echo -e "\e[32m -e "\e[35mheloworld\e[0m"
+echo -e "\e[32m -e "\e[36mhelloworld\e[0m"
 
 
 
