5dcd2ba057c83b470ceba722ce6892908d2d049a
Welcome <jakdevops@gmail.com>
2023-07-26 02:51:58 -0700

    loops

--- loops.sh
+++ loops.sh
@@ -6,4 +6,11 @@ a=10
 while [ $a -gt 0 ]; then
   echo hello
   a=$(($a-1))
+  #break # this command break the loop
   done
+
+
+  #Based on inputs
+  for comp in frontend catalogue user ; do
+   echo installaing component - $comp
+   done
