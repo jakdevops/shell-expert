a59e4a8ef47968fd9380e5ecdbd5e7db7343c242
Welcome <jakdevops@gmail.com>
2023-07-26 02:29:13 -0700

    substitution variables

--- variables.sh
+++ variables.sh
@@ -1,2 +1,18 @@
 a=10
 echo a is $a
+
+#special varaiable
+#$0 - $N, $*, $#
+
+#substitution variables
+#command substitution
+
+DATE=$(date)
+
+echo Today date is $DATE
+
+#Arthimetic substitution
+
+ADD=$(( 2+2 ))
+echo ADD of 2+2 = $ADD
+
